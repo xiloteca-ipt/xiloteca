e3b4732540565786f67fa07459eb75e53f951315
Sócrates Lopes <lopes.socrates@hotmail.com>
2023-07-01 20:34:45 -0400

    Remove platform.sh references in text

--- hello/views.py
+++ hello/views.py
@@ -5,6 +5,6 @@ from django.template import loader
 def index(request):
     template = loader.get_template('base.html')
     context = {
-        'message': "Hello, and welcome to the Platform.sh world",
+        'message': "Hello, world!",
     }
     return HttpResponse(template.render(context, request))
