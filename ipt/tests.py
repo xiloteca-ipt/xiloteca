2ed9c4f7a07c98a154e57afbdd00c5fdcebaf054
Sócrates Lopes <lopes.socrates@hotmail.com>
2023-07-03 18:51:04 -0400

    Add template for test cases

--- ipt/tests.py
+++ ipt/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+class DummyTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        print("setUpTestData: Run once to set up non-modified data for all class methods.")
+
+    def setUp(self):
+        print("setUp: Run once for every test method to setup clean data.")
+
+    def test_false_is_false(self):
+        print("Method: test_false_is_false.")
+        self.assertFalse(False)
