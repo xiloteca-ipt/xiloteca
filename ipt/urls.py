1f3065f2b254a7c29aa56d0be54fdef7605262a0
Sócrates Lopes <lopes.socrates@hotmail.com>
2023-07-03 19:04:18 -0400

    Add default pages to the project

--- ipt/urls.py
+++ ipt/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('busca', views.search, name='search'),
+    path('resultados', views.results, name='results'),
+    path('madeira', views.details, name='details')
 ]
