ec49cfba0cc94b5d5c8f6cbc6454e1d617e40621
Sócrates Lopes <lopes.socrates@hotmail.com>
2023-08-19 11:33:30 -0300

    Add initial tests

--- ipt/tests.py
+++ ipt/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
+from . import models
+from django.apps import apps
 
-class DummyTest(TestCase):
+class ModelsTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         print("setUpTestData: Run once to set up non-modified data for all class methods.")
@@ -8,6 +10,40 @@ class DummyTest(TestCase):
     def setUp(self):
         print("setUp: Run once for every test method to setup clean data.")
 
-    def test_false_is_false(self):
-        print("Method: test_false_is_false.")
-        self.assertFalse(False)
+    def test_madeira_names_are_properly_converted_to_string(self):
+        #arrange
+        expected_madeira_name = 'Madeira Teste'
+        madeira_instance = models.Madeira(cod_madeira=1)
+        madeira = models.MadeiraNomePopular(cod_madeira=madeira_instance, nome_popular=expected_madeira_name)
+
+        #act
+        madeira_name = str(madeira)
+
+        #assert
+        self.assertIsInstance(madeira, models.MadeiraNomePopular)
+        self.assertEqual(expected_madeira_name, madeira_name)
+
+    def test_base_template_is_present(self):
+        #arrange
+        file_path = 'ipt/templates/base.html'
+
+        #act
+        file = None
+        try:
+            file = open(file_path, "r", encoding='UTF-8')
+
+        except FileNotFoundError:
+            print('Could not find file for base template')
+
+        #assert
+        self.assertIsNotNone(file)
+
+    def test_models_are_registered(self):
+        #arrange
+        app_models = []
+
+        #act
+        app_models = apps.get_models()
+
+        #assert
+        self.assertTrue(models.Conteudo in app_models)
