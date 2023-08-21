a42b744bd74f07dbef0ec57fbed68a80bebf913c
JulioVieira0777 <135927451+JulioVieira0777@users.noreply.github.com>
2023-08-21 14:50:38 -0300

    Removendo devido a Add manual por Upload

--- persons/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class PersonsConfig(AppConfig):
-    #default_auto_field = 'django.db.models.AutoField'
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'persons'
